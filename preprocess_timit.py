b23aa2859662c931c46880e6e93c18b2b39ef9b0
Roberto Estevão <robertomest@poli.ufrj.br>
2016-10-21 19:38:34 -0200

    Added preprocessing and training script for TIMIT dataset. Example script renamed to example_ctc.py

--- preprocess_timit.py
+++ preprocess_timit.py
@@ -2,6 +2,8 @@ import librosa
 import re
 import numpy as np
 from keras.preprocessing.sequence import pad_sequences
+import h5py
+import time
 
 def remove_ext(file_list):
     return [f[:-4] for f in file_list]
@@ -19,18 +21,24 @@ def preprocess_audio(audio_path):
     dd_e = np.log(librosa.feature.rmse(S=dd))
     return np.vstack((S, d, dd, S_e, d_e, dd_e)).T, S.shape[1]
 
-dict_char = {chr(value + ord('a')): (value) for value in xrange(ord('z') - ord('a') + 1)}
-dict_char[' '] = len(dict_char)
-dict_char["'"] = len(dict_char)
-dict_char['"'] = len(dict_char)
-dict_char['-'] = len(dict_char)
-dict_char[','] = len(dict_char)
-dict_char['.'] = len(dict_char)
-dict_char['?'] = len(dict_char)
-dict_char[':'] = len(dict_char)
-dict_char[';'] = len(dict_char)
+def normalize_audio(input_list, mean, std):
+    return [(i - mean)/std for i in input_list]
 
-def preprocess_char(transcript_path, row):
+def get_char_map():
+    dict_char = {chr(value + ord('a')): (value) for value in xrange(ord('z') - ord('a') + 1)}
+    dict_char[' '] = len(dict_char)
+    dict_char["'"] = len(dict_char)
+    dict_char['"'] = len(dict_char)
+    dict_char['-'] = len(dict_char)
+    dict_char[','] = len(dict_char)
+    dict_char['.'] = len(dict_char)
+    dict_char['!'] = len(dict_char)
+    dict_char['?'] = len(dict_char)
+    dict_char[':'] = len(dict_char)
+    dict_char[';'] = len(dict_char)
+    return dict_char
+
+def preprocess_char(transcript_path, dict_char, row):
     with open(transcript_path, 'rb') as f:
         txt = f.readlines()[0]
     txt = ' '.join(txt.strip().split(' ')[2:]).lower()
@@ -39,58 +47,125 @@ def preprocess_char(transcript_path, row):
     char_len = values.size
     return (values, indices, char_len)
 
-def preprocess_phn(phn_path):
+
+def get_phn_map(map_path):
+    with open(map_path, 'rb') as f:
+        txt = f.readlines()
+    phn_list = [l.strip().split('\t') for l in txt]
+    phn_map = {p[0]:p[2] for p in phn_list if len(p) == 3}
+    dict_phn = {k: v for v, k in enumerate(set(phn_map.values()))}
+    return phn_map, dict_phn
+
+def preprocess_phn(phn_path, phn_map, dict_phn, row):
     with open(phn_path, 'rb') as f:
         txt = f.readlines()
-    phn_list = [t.strip().split(' ')[2] for t in txt]
+    phn_list = [(l.strip().split(' ')[2]).lower() for l in txt]
+    phn_list = [phn_map[p] for p in phn_list if p in phn_map.keys()]
+    values = np.array([dict_phn[p] for p in phn_list], dtype='int32')
+    indices = np.hstack((row * np.ones((values.size,1)), np.arange(values.size)[:, None])).astype('int64')
+    phn_len = values.size
+    return (values, indices, phn_len)
 
 
-dataset_dir = 'timit'
+if __name__ == '__main__':
+    dataset_dir = 'timit'
 
-files = librosa.util.find_files(dataset_dir)
+    files = librosa.util.find_files(dataset_dir)
 
-# Ignoring SA sentences
-files = [f for f in files if re.search(r"SA([0-9])\.WAV", f) is None]
+    # Ignoring SA sentences
+    files = [f for f in files if re.search(r"SA([0-9])\.WAV", f) is None]
 
-train_files = [f for f in files if f.find('TRAIN') + 1]
-test_valid_files = [f for f in files if f.find('TEST') + 1]
+    train_files = [f for f in files if f.find('TRAIN') + 1]
+    test_valid_files = [f for f in files if f.find('TEST') + 1]
 
-test_spk_list = [s.strip().upper() for s in open('timit/test_spk.list', 'rb').readlines()]
-test_files = [f for f in test_valid_files if any(s in f for s in test_spk_list)]
+    test_spk_list = [s.strip().upper() for s in open('timit/test_spk.list', 'rb').readlines()]
+    test_files = [f for f in test_valid_files if any(s in f for s in test_spk_list)]
 
-valid_spk_list = [s.strip().upper() for s in open('timit/dev_spk.list', 'rb').readlines()]
-valid_files = [f for f in test_valid_files if any(s in f for s in valid_spk_list)]
+    valid_spk_list = [s.strip().upper() for s in open('timit/dev_spk.list', 'rb').readlines()]
+    valid_files = [f for f in test_valid_files if any(s in f for s in valid_spk_list)]
 
-# Remove extension
-train_files = remove_ext(train_files)
-test_files = remove_ext(test_files)
-valid_files = remove_ext(valid_files)
+    # Remove extension
+    train_files = remove_ext(train_files)
+    test_files = remove_ext(test_files)
+    valid_files = remove_ext(valid_files)
 
-print('Got all file names.')
-print('Generating Tensors...')
-for file_list in [train_files, test_files, valid_files]:
+    print('Got all file names.')
 
+    dict_char = get_char_map()
+    phn_map, dict_phn = get_phn_map('timit/phones.60-48-39.map')
 
-    # input_list = []
-    # seq_len_list = []
-    char_values_list, char_indices_list, char_len_list = [], [], []
-    for row, f in enumerate(file_list):
-        # WAV
-    #     features, seq_len = preprocess_audio(f + '.WAV')
-    #     input_list.append(features)
-    #     seq_len_list.append(seq_len)
+    print('Got the dictionaries.')
 
-        # CHAR
-        char_values, char_indices, char_len =  preprocess_char(f + '.TXT', row)
-        char_values_list.append(char_values)
-        char_indices_list.append(char_indices)
-        char_len_list.append(char_len)
-    #
-    # X = pad_sequences(input_list, dtype='float32', padding='post')
+    print('Generating Tensors...')
+    mean, std = None, None
+    with h5py.File('timit.h5', 'w') as h5_file:
+        for file_list, name in zip([train_files, valid_files, test_files], ['train', 'valid', 'test']):
+            t = time.time() # measure time
 
-    # CHAR
-    char_values = np.hstack(char_values_list)
-    char_indices = np.vstack(char_indices_list)
-    char_shape = np.array((len(file_list), np.max(char_len_list)))
-    print('Finished one.')
-print('Generation complete.')
+            input_list, seq_len_list = [], []
+            char_values_list, char_indices_list, char_len_list = [], [], []
+            phn_values_list, phn_indices_list, phn_len_list = [], [], []
+            for row, f in enumerate(file_list):
+                # WAV
+                features, seq_len = preprocess_audio(f + '.WAV')
+                input_list.append(features)
+                seq_len_list.append(seq_len)
+
+                # CHAR
+                char_values, char_indices, char_len =  preprocess_char(f + '.TXT', dict_char, row)
+                char_values_list.append(char_values)
+                char_indices_list.append(char_indices)
+                char_len_list.append(char_len)
+
+                # PHN
+                phn_values, phn_indices, phn_len = preprocess_phn(f + '.PHN', phn_map, dict_phn, row)
+                phn_values_list.append(phn_values)
+                phn_indices_list.append(phn_indices)
+                phn_len_list.append(phn_len)
+
+            # WAV
+            if mean is None or std is None:
+                input_stack = np.vstack(input_list)
+                mean = input_stack.mean(axis=0)
+                std = input_stack.std(axis = 0)
+                # Do not normalize dims with very small std
+                std[std < 1e-9] = 1.
+
+            input_list = normalize_audio(input_list, mean, std)
+
+            inputs = pad_sequences(input_list, dtype='float32', padding='post')
+            seq_len = np.hstack(seq_len_list)
+
+            # CHAR
+            char_values = np.hstack(char_values_list)
+            char_indices = np.vstack(char_indices_list)
+            char_shape = np.array((len(file_list), np.max(char_len_list)))
+
+            # PHN
+            phn_values = np.hstack(phn_values_list)
+            phn_indices = np.vstack(phn_indices_list)
+            phn_shape = np.array((len(file_list), np.max(phn_len_list)))
+
+            # Save to HDF5
+            group = h5_file.create_group(name)
+
+            # inputs
+            inp_grp = group.create_group('inputs')
+            inp_grp.create_dataset('data', data=inputs)
+            inp_grp.create_dataset('seq_len', data=seq_len)
+
+            # character labels
+            char_grp = group.create_group('char')
+            char_grp.create_dataset('values', data=char_values)
+            char_grp.create_dataset('indices', data=char_indices)
+            char_grp.create_dataset('shape', data=char_shape)
+
+            # phoneme labels
+            phn_grp = group.create_group('phn')
+            phn_grp.create_dataset('values', data=phn_values)
+            phn_grp.create_dataset('indices', data=phn_indices)
+            phn_grp.create_dataset('shape', data=phn_shape)
+
+            print('Finished %s in %.fs.' %(name, time.time() - t))
+            h5_file.flush()
+    print('Generation complete.')
--- train_ctc.py
+++ train_ctc.py
@@ -3,66 +3,71 @@
 
 import tensorflow as tf
 # Import CTC loss
-try:
-    from tensorflow.contrib.warpctc.ctc_ops import warp_ctc_loss as ctc_loss
-except ImportError:
-    from tensorflow.python.ops.ctc_ops import ctc_loss
+# try:
+#     from tensorflow.contrib.warpctc.ctc_ops import warp_ctc_loss as ctc_loss
+#     print('Using warp ctc :)')
+# except ImportError:
+from tensorflow.python.ops.ctc_ops import ctc_loss
+print('Using tf ctc :(')
 
 import numpy as np
 import librosa
 from scipy import sparse
+import h5py
 
 import keras
 import keras.backend as K
 from keras.models import Model
-from keras.layers import Input, Dense, Activation, Lambda, TimeDistributed
-from keras.optimizers import RMSprop
+from keras.layers import Input, Dense, Activation, Lambda, TimeDistributed, LSTM
+from keras.optimizers import RMSprop, SGD
 from keras.utils.data_utils import get_file
 from layers import RHN
 
-def ctc_lambda_func(args):
-    y_pred, labels, input_length = args
-    return ctc_loss(tf.transpose(y_pred, perm=[1, 0, 2]), tf.sparse_reorder(labels), input_length[:, 0])
+from preprocess_timit import get_char_map, get_phn_map
 
-def preprocess_audio(audio_path):
-    y, sr = librosa.load(audio_path)
-    S = librosa.feature.melspectrogram(y, sr=sr, hop_length=int(1e-2*sr), n_fft=int(25e-3*sr), n_mels=40)
-    d = librosa.feature.delta(S)
-    dd = librosa.feature.delta(S, order=2)
-    S_e = np.log(librosa.feature.rmse(S=S))
-    d_e = np.log(librosa.feature.rmse(S=d))
-    dd_e = np.log(librosa.feature.rmse(S=dd))
-    return np.vstack((S, d, dd, S_e, d_e, dd_e)).T[None, :], np.array([S.shape[1]])
+def get_inv_dict(dict_label):
+    inv_dict = {v: k for (k, v) in dict_label.iteritems()}
+    # Add blank label
+    inv_dict[len(inv_dict)] = '<b>'
+    return inv_dict
 
-dict_char = {chr(value + ord('a')): (value) for value in xrange(ord('z') - ord('a') + 1)}
-dict_char[' '] = len(dict_char)
-dict_char['.'] = len(dict_char)
-
-def preprocess_transcript(transcript_path):
-    with open(transcript_path, 'rb') as f:
-        txt = f.readlines()[0]
-    txt = ' '.join(txt.strip().split(' ')[2:]).lower()
-    values = np.array([dict_char[c] for c in txt], dtype='int32')
-    # indices = np.hstack((np.zeros((values.size,1)), np.arange(values.size)[:, None])).astype('int64')
-    indices = (np.zeros((values.size)), np.arange(values.size))
-    shape = np.array([1, values.size], dtype='int64')
-    return sparse.coo_matrix((values, indices), shape=shape).tolil()
-
-inv_dict = {v: k for (k, v) in dict_char.iteritems()}
-inv_dict[len(inv_dict)] = 'B'
 def get_output(model, x):
     return ''.join([inv_dict[i] for i in model.predict(x).argmax(axis=2)[0]])
 
+def get_from_h5(h5_file, dataset, label_type='phn'):
+    X = np.array(h5_file['%s/inputs/data' %dataset])
+    seq_len = np.array(h5_file['%s/inputs/seq_len' %dataset])
+
+    values = np.array(h5_file['%s/%s/values' %(dataset, label_type)])
+    indices = np.array(h5_file['%s/%s/indices' %(dataset, label_type)])
+    indices = (indices[:, 0], indices[:, 1])
+    shape = np.array(h5_file['%s/%s/shape' %(dataset, label_type)])
+
+    y = sparse.coo_matrix((values, indices), shape=shape).tolil()
+    return X, seq_len, y
+
+def ctc_lambda_func(args):
+    y_pred, labels, input_length = args
+    return ctc_loss(tf.transpose(y_pred, perm=[1, 0, 2]), labels, input_length[:, 0])
+
 if __name__ == '__main__':
 
-    audio_path = get_file('LDC93S1.wav', origin='https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.wav')
-    transcript_path = get_file('LDC93S1.txt', origin='https://catalog.ldc.upenn.edu/desc/addenda/LDC93S1.txt')
+    label_type = 'phn'
+    # Read dataset
+    with h5py.File('timit.h5', 'r') as f:
+        X, seq_len, y = get_from_h5(f, 'train', label_type=label_type)
+        X_valid, seq_len_valid, y_valid = get_from_h5(f, 'valid', label_type=label_type)
+        X_test, seq_len_test, y_test = get_from_h5(f, 'test', label_type=label_type)
 
-    X, X_length = preprocess_audio(audio_path)
-    y = preprocess_transcript(transcript_path)
+    if label_type == 'phn':
+        _, dict_label = get_phn_map('timit/phones.60-48-39.map')
+    else:
+        dict_label = get_char_map()
+
+    inv_dict = get_inv_dict(dict_label)
 
     nb_features = X.shape[2]
-    nb_classes = len(dict_char) + 1 # Blank
+    nb_classes = len(inv_dict)
 
     ctc = Lambda(ctc_lambda_func, output_shape=(1,), name="ctc")
 
@@ -72,7 +77,11 @@ if __name__ == '__main__':
     input_length = Input(name='input_length', shape=(None,), dtype='int32')
 
     # Define model
-    o = RHN(250, return_sequences=True)(x)
+    nb_hidden = int(np.ceil(250*np.sqrt(2)))
+    o = RHN(nb_hidden, nb_layers=2, return_sequences=True, layer_norm=True)(x)
+    # o = LSTM(250, return_sequences=True)(x)
+    # o = LSTM(250, return_sequences=True)(o)
+    # o = LSTM(250, return_sequences=True)(o)
     o = TimeDistributed(Dense(nb_classes))(o)
     # Define loss as a layer
     l = ctc([o, labels, input_length])
@@ -81,9 +90,12 @@ if __name__ == '__main__':
     pred = Model(input=x, output=o)
 
     # Optimization
-    opt = RMSprop(lr=0.001, clipnorm=10)
+    # opt = RMSprop(lr=0.001, clipnorm=10)
+    opt = SGD(lr=1e-5, momentum=0.9, clipnorm=10.)
     # Compile with dummy loss
     model.compile(loss={'ctc': lambda y_true, y_pred: y_pred}, optimizer=opt)
 
     # FITZERA PESADAO
-    # model.fit([X, y, X_length], np.zeros((1,)), batch_size=1, nb_epoch=500)
+    model.fit([X, y, seq_len], np.zeros((X.shape[0],)),
+              batch_size=64, nb_epoch=10,
+              validation_data=([X_valid, y_valid, seq_len_valid], np.zeros((X_valid.shape[0],))))
