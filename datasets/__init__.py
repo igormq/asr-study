264de88c1a64c4e92d48418ddd3c2c568432fac9
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2017-01-19 15:23:41 -0200

    import childrens of DatasetParser on init

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -2,5 +2,10 @@ from common.utils import safe_mkdirs
 import os
 
 DT_ABSPATH = os.path.join(os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.path.sep)[:-1]), '.datasets')
+safe_mkdirs(DT_ABSPATH)
 
-safe_makedirs(DT_ABSPATH)
+from datasets.dataset_parser import DatasetParser
+from datasets.sidney import Sidney
+from datasets.lapsbm import LapsBM
+from datasets.voxforge import VoxForge
+from datasets.cslu_spoltech_port import CSLUSpoltechPort as CSLU
