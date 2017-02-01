5aa3f138bb4255b00fa009570439cd412566e857
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2017-02-01 10:16:08 -0200

    Added variational dropout model for ctc (vardropout)

--- core/models.py
+++ core/models.py
@@ -8,7 +8,8 @@ from common.hparams import HParams
 import keras
 from keras.initializations import uniform
 from keras.models import Model
-from keras.layers import Input, GaussianNoise, TimeDistributed, Dense, LSTM, Masking, Bidirectional, Lambda
+from keras.layers import Input, GaussianNoise, TimeDistributed, Dense, LSTM, Masking, Bidirectional, Lambda, Dropout
+from keras.regularizers import l1, l2, l1l2
 
 def ctc_model(input_, output):
     ''' Given the output returns a model appending ctc_loss and the decoder
@@ -33,8 +34,7 @@ def graves2006(hparams=None):
     params = HParams(nb_features=26,
                      nb_hidden=100,
                      nb_classes=28,
-                     std=.6,
-                     lr=0.01)
+                     std=.6)
 
     params.parse(hparams)
 
@@ -46,5 +46,66 @@ def graves2006(hparams=None):
                       return_sequences=True,
                       consume_less='gpu'))(o)
     o = TimeDistributed(Dense(params.nb_classes))(o)
+
+    return ctc_model(x, o)
+
+def graves2012m(hparams=None):
+    params = HParams(nb_features=26,
+                     nb_classes=28,
+                     nb_hidden=250,
+                     nb_layers=3,
+                     reg_W=0,
+                     reg_U=0,
+                     drop_W=0,
+                     drop_U=0)
+
+    params.parse(hparams)
+
+    x = Input(name='input', shape=(None, params.nb_features))
+    o = x
+
+    for _ in range(params.nb_layers):
+        o = Bidirectional(LSTM(params.nb_hidden,
+                          return_sequences=True,
+                          W_regularizer=l2(params.reg_W),
+                          U_regularizer=l2(params.reg_U),
+                          dropout_W=params.drop_W,
+                          dropout_U=params.drop_U,
+                          consume_less='gpu'))(o)
+
+    o = TimeDistributed(Dense(params.nb_classes))(o)
+
+    return ctc_model(x, o)
+
+def vardropout(hparams)
+    params = HParams(nb_features=39,
+                     nb_classes=28,
+                     nb_hidden=256,
+                     nb_layers=3,
+                     dropout=0.25,
+                     input_dropout=True,
+                     weight_decay=1e-4)
+
+    params.parse(hparams)
+
+    x = Input(name='input', shape=(None, params.nb_features))
+    o = x
+
+    if params.input_dropout:
+        o = Dropout(params.dropout)(o)
+
+    for _ in range(params.nb_layers):
+        o = Bidirectional(LSTM(params.nb_hidden,
+                          return_sequences=True,
+                          W_regularizer=l2(params.weight_decay),
+                          U_regularizer=l2(params.weight_decay),
+                          b_regularizer=l2(params.weight_decay),
+                          dropout_W=params.dropout,
+                          dropout_U=params.dropout,
+                          consume_less='gpu'))(o)
+
+    o = TimeDistributed(Dense(params.nb_classes,
+                              W_regularizer=l2(params.weight_decay,
+                              b_regularizer=l2(params.weight_decay))))(o)
 
     return ctc_model(x, o)
