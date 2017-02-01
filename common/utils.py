c335516b0d8feb3251fdeac9f982568a1a1edef5
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2017-02-01 16:42:20 -0200

    Moving config_gpu to common/utils Adding get_from_module Renaming some args

--- common/utils.py
+++ common/utils.py
@@ -8,23 +8,16 @@ import os
 
 import numpy as np
 from scipy import sparse
+
+
+import keras.backend as K
+import tensorflow as tf
+
 import keras
 
 import inspect
 
 import core
-
-def get_from_h5(h5_file, dataset, label_type='char'):
-    X = np.array(h5_file['%s/inputs/data' %dataset])
-    seq_len = np.array(h5_file['%s/inputs/seq_len' %dataset])
-
-    values = np.array(h5_file['%s/%s/values' %(dataset, label_type)])
-    indices = np.array(h5_file['%s/%s/indices' %(dataset, label_type)])
-    indices = (indices[:, 0], indices[:, 1])
-    shape = np.array(h5_file['%s/%s/shape' %(dataset, label_type)])
-
-    y = sparse.coo_matrix((values, indices), shape=shape).tolil()
-    return X, seq_len, y
 
 def safe_mkdirs(path):
     ''' Safe makedirs
@@ -42,23 +35,51 @@ def safe_mkdirs(path):
 
     return path
 
-def get_functions_from_module(module):
-    return dict(inspect.getmembers(sys.modules[module], lambda member: inspect.isfunction(member) and member.__module__ == module))
+def get_from_module(module, name):
+    members = dict(inspect.getmembers(sys.modules[module], lambda member: hasattr(member, '__module__') and member.__module__ == module))
+
+    if name is None or name.lower() == 'none':
+        return None
+
+    members = {k.lower().strip(): v for k, v in members.items()}
+
+    try:
+        return members[name.lower().strip()]
+    except KeyError, e:
+        raise KeyError, "%s not found in %s.\n Valid values are: %s" % (name, module, ', '.join(members.keys()))
 
 def get_custom_objects():
+    """ Verify all custom object that may be used to load a keras model
+    """
     all_custom_objects = []
-
-    for module in ['core.layers', 'core.metrics', 'core.ctc_utils']:
+    for module in ['core.layers', 'core.layers_utils', 'core.metrics', 'core.ctc_utils']:
         all_custom_objects.extend(inspect.getmembers(sys.modules[module], lambda member: (inspect.isclass(member) or inspect.isfunction(member)) and member.__module__ == module))
 
     return dict(all_custom_objects)
 
-def model_loader(treta_path):
-    modelin = keras.models.load_model(treta_path, custom_objects=get_custom_objects())
-    return modelin
+def model_loader(model_fname):
+    """ Loading keras model with custom objects
+    """
+    model = keras.models.load_model(model_fname, custom_objects=get_custom_objects())
+    return model
 
 def ld2dl(ld):
     '''Transform a list of dictionaries in a dictionaries with lists
-    NOTE: All dictionaries have the same keys
+    # Note
+        All dictionaries have the same keys
     '''
     return dict(zip(ld[0],zip(*[d.values() for d in ld])))
+
+def config_gpu(gpu, allow_growth=False):
+    # Choosing gpu
+    if gpu == '-1':
+        config = tf.ConfigProto(device_count = {'GPU': 0})
+    else:
+        if gpu == 'all':
+            gpu = ''
+        config = tf.ConfigProto()
+        config.gpu_options.visible_device_list = gpu
+    if allow_growth == True: # dynamic gpu memory allocation
+        config.gpu_options.allow_growth = True
+    session = tf.Session(config=config)
+    K.set_session(session)
--- train.py
+++ train.py
@@ -18,7 +18,7 @@ from core.callbacks import MetaCheckpoint
 
 from preprocessing import audio, text
 
-from common.utils import get_functions_from_module
+import common.utils as utils
 from common.dataset_generator import DatasetGenerator
 from common.hparams import HParams
 
@@ -27,65 +27,57 @@ import uuid
 import os
 import json
 import datetime
+import inspect
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Training an ASR system.')
 
+    # Model settings
     parser.add_argument('--model', default='graves2006', type=str)
-    parser.add_argument('--params', default='{}', type=str)
+    parser.add_argument('--model_params', default='{}', type=str)
 
+    # Hyper parameters
     parser.add_argument('--nb_epoch', default=100, type=int)
+    parser.add_argument('--lr', default=0.01, type=float)
+    parser.add_argument('--momentum', default=0.9, type=float)
+    parser.add_argument('--clipnorm', default=5, type=float)
+    parser.add_argument('--batch_size', default=32, type=int)
+    parser.add_argument('--opt', default='sgd', type=str, choices=['sgd', 'adam']) #optimizer
+    # End of hyper parameters
 
+    # Dataset definitions
     parser.add_argument('--dataset', default=None, type=str, help="Path to dataset h5 or json file separated by train/valid/test")
-
+    # If --dataset was not defined
     parser.add_argument('--train', type=str)
     parser.add_argument('--test', type=str, default=None)
     parser.add_argument('--valid', type=str, default=None)
 
-    parser.add_argument('--split', type=float, default=.2, help='Split valid/train ratio. Only enabled when valid=None')
-
-    parser.add_argument('--lr', default=0.01, type=float)
-
-    parser.add_argument('--lr_schedule', default='ReduceLROnPlateau')
-    parser.add_argument('--lr_params', default="{'monitor': 'val_loss', 'factor':0.1, 'patience':5, 'min_lr':1e-6}")
-
-    parser.add_argument('--momentum', default=0.9, type=float)
-    parser.add_argument('--clipnorm', default=5, type=float)
-    parser.add_argument('--batch_size', default=32, type=int)
-    parser.add_argument('--opt', default='sgd', type=str, choices=['sgd', 'adam'])
-
+    # Features generation (if necessary)
     parser.add_argument('--feats', type=str, default='raw', choices=['mfcc', 'raw', 'logfbank'])
     parser.add_argument('--feats_params', type=str, default='{}')
 
+    # Label generation (if necessary)
     parser.add_argument('--text_parser', type=str, default='simple_char_parser')
+    parser.add_argument('--text_parser_params', type=str, default='{}')
 
+    # Callbacks
+    parser.add_argument('--lr_schedule', default=None)
+    parser.add_argument('--lr_params', default="{'monitor': 'val_loss', 'factor':0.1, 'patience':5, 'min_lr':1e-6}")
+
+    # Other configs
     parser.add_argument('--save', default=None, type=str)
-
     parser.add_argument('--gpu', default='0', type=str)
     parser.add_argument('--allow_growth', default=False, action='store_true')
 
     args = parser.parse_args()
 
-    # Choosing gpu
-    if args.gpu == '-1':
-        config = tf.ConfigProto(device_count = {'GPU': 0})
-    else:
-        if args.gpu == 'all':
-            args.gpu = ''
-        config = tf.ConfigProto()
-        config.gpu_options.visible_device_list = args.gpu
-    if args.allow_growth == True:
-        config.gpu_options.allow_growth = True
-    session = tf.Session(config=config)
-    K.set_session(session)
+    # GPU configuration
+    utils.config_gpu(args.gpu, args.allow_growth)
 
     # Recovering all valid models
-    valid_models = get_functions_from_module('core.models')
-    if args.model not in valid_models.keys():
-        raise ValueError('model %s not found. Valid models are: %s' % (args.model, ', '.join(valid_models.keys())))
-
-    # Load model
-    model = valid_models[args.model](args.params)
+    model_fn = utils.get_from_module('core.models', args.model)
+    # Loading model
+    model = model_fn(args.model_params)
 
     # Optimization
     if args.opt == 'sgd':
@@ -98,6 +90,7 @@ if __name__ == '__main__':
                         'decoder': decoder_dummy_loss},
                   optimizer=opt, metrics={'decoder': metrics.ler},
                   loss_weights=[1, 0])
+
 
     # Creating the results folder
     name = args.save
@@ -113,45 +106,37 @@ if __name__ == '__main__':
     callback_list = [meta_ckpt, model_ckpt, best_ckpt]
 
     # LR schedules
-    lr_params = HParams()
-    lr_params.parse(args.lr_params)
-    if args.lr_schedule is not None and args.lr_schedule == 'ReduceLROnPlateau':
-        reduce_lr = ReduceLROnPlateau(*lr_params.keyvals)
-        callback_list.append(reduce_lr)
+    lr_schedule_fn = utils.get_from_module('keras.callbacks', args.lr_schedule)
+    if lr_schedule_fn:
+        lr_schedule = lr_schedule_fn(*HParams(from_str=args.lr_params).values())
+        callback_list.append(lr_schedule)
 
     # Features extractor
-    feats_params = HParams()
-    feats_params.parse(args.feats_params)
-    if args.feats == 'mfcc':
-        feats_extractor = audio.MFCC(*feats_params.keyvals)
-    elif args.feats == 'logfbank':
-        feats_extractor = audio.LogFbank(*feats_params.keyvals)
-    else:
-        feats_extractor = None
+    feats_extractor = utils.get_from_module('preprocessing.audio', args.feats)
+    if feats_extractor and not inspect.isfunction(feats_extractor):
+        feats_extractor = feats_extractor(*HParams(from_str=args.feats_params).values())
 
     # Recovering text parser
-    valid_text_parsers = get_functions_from_module('preprocessing.text')
-    if not valid_text_parsers.has_key(args.text_parser):
-        raise ValueError('text_parser %s not found. Valid text_parser are: %s' % (args.text_parser, ', '.join(valid_text_parsers.keys())))
-
-    text_parser = valid_text_parsers[args.text_parser]
+    text_parser = utils.get_from_module('preprocessing.text', args.text_parser)
+    if text_parser and not inspect.isfunction(text_parser):
+        text_parser = text_parser(*HParams(from_str=(args.text_parser_params)).values())
 
     # Data generator
-    data_gen = DatasetGenerator(feats_extractor, text_parser)
-
+    data_gen = DatasetGenerator(feats_extractor, text_parser, batch_size=args.batch_size, seed=0)
+    # iterators over datasets
     train_flow, valid_flow, test_flow = None, None, None
 
     if args.dataset is not None:
-        train_flow, valid_flow, test_flow = data_gen.flows_from_fname(args.dataset, batch_size=args.batch_size, seed=0)
+        train_flow, valid_flow, test_flow = data_gen.flows_from_fname(args.dataset)
     else:
         if args.train:
-            train_flow = data_gen.flow_from_fname(args.train, dt_name='train', batch_size=args.batch_size, seed=0)
+            train_flow = data_gen.flow_from_fname(args.train, dt_name='train')
 
         if args.valid:
-            valid_flow = data_gen.flow_from_fname(args.valid, dt_name='valid', batch_size=args.batch_size, seed=0)
+            valid_flow = data_gen.flow_from_fname(args.valid, dt_name='valid')
 
         if args.test:
-             test_flow = data_gen.flow_from_fname(args.test, dt_name='test', batch_size=args.batch_size)
+             test_flow = data_gen.flow_from_fname(args.test, dt_name='test')
 
     nb_val_samples = None
     if valid_flow:
@@ -162,3 +147,6 @@ if __name__ == '__main__':
 
     if test_flow:
         metrics = model.evaluate_generator(test_flow, test_flow.len, max_q_size=10, nb_worker=1)
+
+        with open(os.path.join(name, 'results.txt')) as f:
+            f.write('Total loss: %.4f\nCTC Loss: %.4f\nLER: %.2f' % (metrics[0], metrics[1], metrics[3]))
