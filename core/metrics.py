7b7305c1c356b4c9bde0095567af27a26c1fa694
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2017-02-15 20:14:13 -0200

    Added first version of wer using Martin Thomas' code

--- core/metrics.py
+++ core/metrics.py
@@ -6,3 +6,55 @@ def ler(y_true, y_pred, **kwargs):
         Label Error Rate. For more information see 'tf.edit_distance'
     """
     return tf.reduce_mean(tf.edit_distance(y_pred, y_true, **kwargs))
+
+def wer(r, h):
+    """
+    Calculation of WER with Levenshtein distance.
+
+    Credits to: Martin Thoma
+    https://martin-thoma.com/word-error-rate-calculation/
+
+    Works only for iterables up to 254 elements (uint8).
+    O(nm) time ans space complexity.
+
+    Parameters
+    ----------
+    r : list
+    h : list
+
+    Returns
+    -------
+    int
+
+    Examples
+    --------
+    >>> wer("who is there".split(), "is there".split())
+    1
+    >>> wer("who is there".split(), "".split())
+    3
+    >>> wer("".split(), "who is there".split())
+    3
+    """
+    # initialisation
+    import numpy
+    d = numpy.zeros((len(r)+1)*(len(h)+1), dtype=numpy.uint8)
+    d = d.reshape((len(r)+1, len(h)+1))
+    for i in range(len(r)+1):
+        for j in range(len(h)+1):
+            if i == 0:
+                d[0][j] = j
+            elif j == 0:
+                d[i][0] = i
+
+    # computation
+    for i in range(1, len(r)+1):
+        for j in range(1, len(h)+1):
+            if r[i-1] == h[j-1]:
+                d[i][j] = d[i-1][j-1]
+            else:
+                substitution = d[i-1][j-1] + 1
+                insertion    = d[i][j-1] + 1
+                deletion     = d[i-1][j] + 1
+                d[i][j] = min(substitution, insertion, deletion)
+
+    return d[len(r)][len(h)]
