c6345e56ce5dbcd346c3cf6355e48773c717c080
Igor Macedo Quintanilha <igormq@poli.ufrj.br>
2017-03-10 21:38:37 -0300

    Fix imports

--- datasets/brsd.py
+++ datasets/brsd.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from datasets import DatasetParser
 from datasets import CSLU, VoxForge, Sid, LapsBM
 
